d917753aca2d7dab1319f7ca1a92f01d8103a5b0
Madelaine Daniela <danu_146@live.com.ar>
2020-05-16 01:03:44 -0300

    ventana agregar
    Diseño de ventana

--- src/presentacion/vista/VentanaPrincipal.java
+++ src/presentacion/vista/VentanaPrincipal.java
@@ -14,6 +14,7 @@ import javax.swing.JTextField;
 //import javax.swing.table.DefaultTableModel;
 
 import daolmpl.Conexion;
+import java.awt.Font;
 
 
 public class VentanaPrincipal {
@@ -64,39 +65,47 @@ public class VentanaPrincipal {
 	private void initialize() 
 	{
 		frmRegistro = new JFrame();
+		frmRegistro.getContentPane().setFont(new Font("Verdana", Font.PLAIN, 13));
 		frmRegistro.setTitle("Programa");
 		frmRegistro.setBounds(100, 100, 517, 353);
 		frmRegistro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmRegistro.getContentPane().setLayout(null);
 		
 		btnAceptar = new JButton("Aceptar");
-		btnAceptar.setBounds(66, 222, 116, 23);
+		btnAceptar.setFont(new Font("Verdana", Font.PLAIN, 13));
+		btnAceptar.setBounds(56, 233, 116, 23);
 		frmRegistro.getContentPane().add(btnAceptar);
 		
 		JLabel lblNombre = new JLabel("Nombre");
-		lblNombre.setBounds(66, 59, 56, 16);
+		lblNombre.setFont(new Font("Verdana", Font.BOLD, 14));
+		lblNombre.setBounds(109, 59, 63, 16);
 		frmRegistro.getContentPane().add(lblNombre);
 		
 		JLabel lblApellido = new JLabel("Apellido");
-		lblApellido.setBounds(66, 117, 56, 16);
+		lblApellido.setFont(new Font("Verdana", Font.BOLD, 14));
+		lblApellido.setBounds(109, 117, 72, 16);
 		frmRegistro.getContentPane().add(lblApellido);
 		
 		JLabel lblDni = new JLabel("Dni");
-		lblDni.setBounds(66, 176, 56, 16);
+		lblDni.setFont(new Font("Verdana", Font.BOLD, 14));
+		lblDni.setBounds(124, 176, 56, 16);
 		frmRegistro.getContentPane().add(lblDni);
 		
 		textNombre = new JTextField();
-		textNombre.setBounds(214, 56, 163, 22);
+		textNombre.setFont(new Font("Verdana", Font.PLAIN, 14));
+		textNombre.setBounds(214, 56, 163, 23);
 		frmRegistro.getContentPane().add(textNombre);
 		textNombre.setColumns(10);
 		
 		textApellido = new JTextField();
+		textApellido.setFont(new Font("Verdana", Font.PLAIN, 14));
 		textApellido.setBounds(214, 114, 163, 22);
 		frmRegistro.getContentPane().add(textApellido);
 		textApellido.setColumns(10);
 		
 		textDni = new JTextField();
-		textDni.setBounds(214, 173, 116, 22);
+		textDni.setFont(new Font("Verdana", Font.PLAIN, 14));
+		textDni.setBounds(214, 173, 163, 22);
 		frmRegistro.getContentPane().add(textDni);
 		textDni.setColumns(10);
 	}
