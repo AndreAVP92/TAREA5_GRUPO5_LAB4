2549f03be522b44a548f6dbd0717fe0ea0d088c0
Madelaine Daniela <danu_146@live.com.ar>
2020-05-16 19:15:47 -0300

    panelEliminar
    panelEliminar

--- src/presentacion/controlador/Controlador.java
+++ src/presentacion/controlador/Controlador.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import negocio.PersonaNegocio;
 import presentacion.vista.PanelAgregarPersonas;
-import presentacion.vista.PanelEliminarPersonas;
+
 import presentacion.vista.VentanaPrincipal;
 import entidad.Persona;
 
@@ -16,7 +16,7 @@ public class Controlador implements ActionListener
 	private PersonaNegocio pNeg;
 	private ArrayList<Persona> personasEnTabla;
 	private PanelAgregarPersonas pnlIngresoPersonas;
-	private PanelEliminarPersonas pnlEliminarPersonas;
+	//private PanelEliminarPersonas pnlEliminarPersonas;
 	
 		//Constructor
 	
@@ -28,13 +28,13 @@ public class Controlador implements ActionListener
 			
 			//Instancio los paneles
 			this.pnlIngresoPersonas = new PanelAgregarPersonas();
-			this.pnlEliminarPersonas = new PanelEliminarPersonas();
+			//this.pnlEliminarPersonas = new PanelEliminarPersonas();
 			
 			//Eventos menu del Frame principal llamado Ventana
 			
 			//ESTE DA EL ERROR//this.ventanaPrincipal.getMenuAgregar().addActionListener(a->EventoClickMenu_AbrirPanel_AgregarPersona(a));
 			
-			this.ventanaPrincipal.getMenuEliminar().addActionListener(a->EventoClickMenu_AbrirPanel_EliminarPersona(a));
+			//this.ventanaPrincipal.getMenuEliminar().addActionListener(a->EventoClickMenu_AbrirPanel_EliminarPersona(a));
 		}
 		
 		//EventoClickMenu abrir PanelAgregarPersonas
@@ -53,15 +53,15 @@ public class Controlador implements ActionListener
 		}
 		
 		//EventoClickMenu abrir PanelEliminarPersonas
-		public void EventoClickMenu_AbrirPanel_EliminarPersona(ActionEvent a)
+		/*public void EventoClickMenu_AbrirPanel_EliminarPersona(ActionEvent a)
 		{		
 			ventanaPrincipal.getContentPane().removeAll();
-			ventanaPrincipal.getContentPane().add(pnlEliminarPersonas);
+			ventanaPrincipal.getContentPane().add();
 			ventanaPrincipal.getContentPane().repaint();
 			ventanaPrincipal.getContentPane().revalidate();
 		}
 		
-		
+		*/
 		public void inicializar()
 		{
 			this.ventanaPrincipal.setVisible(true);
