97fb70a42ab41ec8e7a110d1203323c4dc6c0211
Madelaine Daniela <danu_146@live.com.ar>
2020-05-16 16:59:35 -0300

    Paneles
    navegacion y diseño

--- src/presentacion/controlador/Controlador.java
+++ src/presentacion/controlador/Controlador.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 
 //import java.util.ArrayList;
 import negocio.PersonaNegocio;
+import presentacion.vista.PanelAgregarPersonas;
 import presentacion.vista.VentanaPrincipal;
 import entidad.Persona;
 
@@ -15,6 +16,7 @@ public class Controlador implements ActionListener
 	private VentanaPrincipal ventanaPrincipal;
 	private PersonaNegocio pNeg;
 	private ArrayList<Persona> personasEnTabla;
+	private PanelAgregarPersonas pnlIngresoPersonas;
 	
 		//Constructor
 	
@@ -22,8 +24,32 @@ public class Controlador implements ActionListener
 		{
 			this.ventanaPrincipal = vista;
 			this.pNeg = pNeg;
+			
+			
+			//Instancio los paneles
+			this.pnlIngresoPersonas = new PanelAgregarPersonas();
+			
+			//Eventos menu del Frame principal llamado Ventana
+			
+			//ESTE DA EL ERROR//this.ventanaPrincipal.getMenuAgregar().addActionListener(a->EventoClickMenu_AbrirPanel_AgregarPersona(a));
+			
+			
 		}
 		
+		//EventoClickMenu abrir PanelAgregarPersonas
+		public void  EventoClickMenu_AbrirPanel_AgregarPersona(ActionEvent a)
+		{		
+			ventanaPrincipal.getContentPane().removeAll();
+			ventanaPrincipal.getContentPane().add(pnlIngresoPersonas);
+			ventanaPrincipal.getContentPane().repaint();
+			ventanaPrincipal.getContentPane().revalidate();
+		}
+		
+		private void EventoClickBoton_AgregarPesona_PanelAgregarPersonas(ActionEvent a) {
+			
+			//TERMINAR FUNCION
+		
+		}
 		
 		
 		public void inicializar()
---
+++ src/presentacion/vista/PanelAgregarPersonas.java
@@ -0,0 +1,112 @@
+package presentacion.vista;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class PanelAgregarPersonas extends JPanel {
+	
+	private JTextField textField_Apellido;
+	private JTextField textField_DNI;
+	private JTextField textField_Nombre;
+	
+		
+	public PanelAgregarPersonas() {
+		
+			super();
+			initialize();
+		}
+
+	public JTextField getTextField_Apellido() {
+		return textField_Apellido;
+	}
+
+
+
+
+	public void setTextField_Apellido(JTextField textField_Apellido) {
+		this.textField_Apellido = textField_Apellido;
+	}
+
+
+
+
+	public JTextField getTextField_DNI() {
+		return textField_DNI;
+	}
+
+
+
+
+	public void setTextField_DNI(JTextField textField_DNI) {
+		this.textField_DNI = textField_DNI;
+	}
+
+
+
+
+	public JTextField getTextField_Nombre() {
+		return textField_Nombre;
+	}
+
+
+
+
+	public void setTextField_Nombre(JTextField textField_Nombre) {
+		this.textField_Nombre = textField_Nombre;
+	}
+
+	public void initialize() {
+		
+		this.setBounds(100, 100, 514, 455);
+		setLayout(null);
+		
+		textField_Apellido = new JTextField();
+		textField_Apellido.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		textField_Apellido.setBounds(207, 159, 157, 22);
+		add(textField_Apellido);
+		textField_Apellido.setColumns(10);
+		
+		textField_DNI = new JTextField();
+		textField_DNI.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		textField_DNI.setBounds(207, 242, 157, 22);
+		add(textField_DNI);
+		textField_DNI.setColumns(10);
+		
+		JButton btnAceptar = new JButton("Aceptar");
+		btnAceptar.setBounds(66, 334, 155, 25);
+		add(btnAceptar);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNombre.setBounds(110, 85, 70, 19);
+		add(lblNombre);
+		
+		JLabel lblApellido = new JLabel("Apellido");
+		lblApellido.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblApellido.setBounds(110, 162, 70, 16);
+		add(lblApellido);
+		
+		JLabel lblDNI = new JLabel("DNI");
+		lblDNI.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblDNI.setBounds(124, 245, 56, 16);
+		add(lblDNI);
+		
+		textField_Nombre = new JTextField();
+		textField_Nombre.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		textField_Nombre.setColumns(10);
+		textField_Nombre.setBounds(207, 83, 157, 22);
+		add(textField_Nombre);
+
+	}
+
+	public void show()
+	{
+		this.setVisible(true);
+	}
+
+
+	
+}
