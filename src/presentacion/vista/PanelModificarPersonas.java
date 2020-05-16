e76b3e239060256dd6504ab65c6914a917dfe956
ElianaOcampo <elianaocampo.contacto@gmail.com>
2020-05-16 18:07:51 -0300

    agregar panelModificar

--- src/presentacion/vista/PanelModificarPersonas.java
+++ src/presentacion/vista/PanelModificarPersonas.java
@@ -3,6 +3,8 @@ package presentacion.vista;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JList;
 
 public class PanelModificarPersonas extends JPanel {
 	private JTextField txtNombre;
@@ -14,31 +16,43 @@ public class PanelModificarPersonas extends JPanel {
 		setLayout(null);
 		
 		JLabel lblNombre = new JLabel("Nombre");
-		lblNombre.setBounds(150, 301, 57, 20);
+		lblNombre.setBounds(171, 303, 57, 20);
 		add(lblNombre);
 		
 		txtNombre = new JTextField();
-		txtNombre.setBounds(211, 298, 98, 26);
+		txtNombre.setBounds(232, 300, 98, 26);
 		add(txtNombre);
 		txtNombre.setColumns(10);
 		
 		JLabel lblDni = new JLabel("DNI");
-		lblDni.setBounds(15, 301, 30, 20);
+		lblDni.setBounds(39, 303, 30, 20);
 		add(lblDni);
 		
 		txtDni = new JTextField();
 		txtDni.setColumns(10);
-		txtDni.setBounds(45, 298, 87, 26);
+		txtDni.setBounds(69, 300, 87, 26);
 		add(txtDni);
 		
 		lblApellido = new JLabel("Apellido");
-		lblApellido.setBounds(322, 301, 70, 20);
+		lblApellido.setBounds(343, 303, 70, 20);
 		add(lblApellido);
 		
 		txtApellido = new JTextField();
 		txtApellido.setColumns(10);
-		txtApellido.setBounds(384, 298, 98, 26);
+		txtApellido.setBounds(405, 300, 98, 26);
 		add(txtApellido);
+		
+		JButton btnModificar = new JButton("Modificar");
+		btnModificar.setBounds(518, 293, 115, 41);
+		add(btnModificar);
+		
+		JList list = new JList();
+		list.setBounds(39, 40, 579, 240);
+		add(list);
+		
+		JLabel lblModificar = new JLabel("Seleccione la persona a modificar:");
+		lblModificar.setBounds(39, 16, 327, 20);
+		add(lblModificar);
 
 	}
 
